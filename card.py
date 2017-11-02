e0b9d51dcb546c4e5687ce846cb62c7e0187257e
Emma Westerhoff <ewesterhoff@olin.edu>
2017-11-02 11:36:49 -0400

    add card class

---
+++ card.py
@@ -0,0 +1,5 @@
+class Card():
+    """ Grid world that contains Pauls (and other things) living in cells. """
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
