b9b51ef4abe82a061cc70173791b02446414bd39
alakmazaheri <avalakmazaheri@gmail.com>
2017-11-02 11:42:27 -0400

    Add basic version of card class

--- card.py
+++ card.py
@@ -1,5 +1,5 @@
 class Card():
-    """ Grid world that contains Pauls (and other things) living in cells. """
+    """ Rudimentary card class to track suit and value """
     def __init__(self, suit, value):
         self.suit = suit
         self.value = value
