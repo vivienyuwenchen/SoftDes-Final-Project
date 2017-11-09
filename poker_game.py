67929d426ba84910d0f4ccc4d2eba45688db683b
Harrison Young <hyoung@olin.edu>
2017-11-09 11:46:44 -0500

    spaces for tabs

--- poker_game.py
+++ poker_game.py
@@ -2,24 +2,25 @@ from card import *
 
 poker = Game(10000, 10000)
 while True:
-	poker.newround()
+    poker.newround()
 
-	if len(poker.deck.cards) < 20:
-		poker.add_deck()
+    if len(poker.deck.cards) < 20:
+        poker.add_deck()
 
-	poker.pocket()
-	if poker.player1.folded == False and poker.player1.folded == False:
-		poker.flop()
-		poker.__repr__()
-	if poker.player1.folded == False and poker.player1.folded == False:
-		poker.turn()
-		poker.__repr__()
-	if poker.player1.folded == False and poker.player1.folded == False:
-		poker.river()
-		poker.__repr__()
-	if poker.player1.folded == False and poker.player1.folded == False:
-		poker.showdown()
-	elif poker.player1.folded == True:
-		poker.wins_Hand(poker.player2)
-	else:
-		poker.wins_Hand(poker.player1)
+    poker.pocket()
+
+    if poker.player1.folded == False and poker.player2.folded == False:
+        poker.flop()
+        poker.__repr__()
+    if poker.player1.folded == False and poker.player2.folded == False:
+        poker.turn()
+        poker.__repr__()
+    if poker.player1.folded == False and poker.player2.folded == False:
+        poker.river()
+        poker.__repr__()
+    if poker.player1.folded == False and poker.player2.folded == False:
+        poker.showdown()
+    elif poker.player1.folded == True:
+        poker.wins_Hand(poker.player2)
+    else:
+        poker.wins_Hand(poker.player1)
