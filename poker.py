b687d1212e4f2b241cb59f4c8f06adb93841462c
Harris Davidson <harrisdavidson@Harriss-MacBook-Pro.local>
2017-12-08 21:04:56 -0500

    Resize cards

--- poker.py
+++ poker.py
@@ -35,8 +35,9 @@ class Card(pygame.sprite.Sprite):
         self.screen = screen
         self.image =  pygame.image.load(picture).convert_alpha()
         self.image.set_colorkey(white)
-        self.image.convert_alpha()
+        self.image.convert_alpha() 
         self.rect = self.image.get_rect()
+        self.image = pygame.transform.scale(self.image, (110, 160)) 
 
         self.x = 600
         self.y = 450
@@ -60,6 +61,7 @@ class CardBack(pygame.sprite.Sprite):
         self.image =  pygame.image.load(picture).convert_alpha()
         self.image.set_colorkey(white)
         self.image.convert_alpha()
+        self.image = pygame.transform.scale(self.image, (110, 160)) 
         self.rect = self.image.get_rect()
 
         self.x = 600
